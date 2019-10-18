50d554bf49f647fe950f460bdddfd5b431355f2c
echavis <echavis@umich.edu>
2019-10-17 20:49:23 -0400

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject interactive_nlp_interface "0.1.0-SNAPSHOT"
+(defproject interactive_nlp_interface "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
