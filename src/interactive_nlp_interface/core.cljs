a7bfbd6f80457b043da04e1d3e03470dc579c73b
echavis <echavis@umich.edu>
2020-07-20 02:03:09 -0400

    Add evaluation page
    We should show users the original sentence, as well
    as the relations drawn from the re-write and the original.

--- src/interactive_nlp_interface/core.cljs
+++ src/interactive_nlp_interface/core.cljs
@@ -19,10 +19,16 @@
 (defn submit-sentence-path []
   (get-url @host "/api/submit-sentence/"))
 
+(defn get-next-evaluation-path []
+  (get-url @host "/api/get-next-evaluation-in-experiment/"))
+(defn submit-evaluation-path []
+  (get-url @host "/api/submit-evaluation/"))
+
 (def experiment "testing-1")
 
 (defonce extracted-relations (r/atom ()))
 (defonce current-benchmark-sentence (r/atom {}))
+(defonce current-evaluation (r/atom {}))
 
 (defn display-relation [relation]
   (str (:subject relation) " - " (:relation relation) " - " (:object relation)))
@@ -86,7 +92,48 @@
   (submit-sentence! (assoc @current-benchmark-sentence :sentence (get-user-input))
                     experiment))
 
+(defn update-current-evaluation! [experiment-id]
+  (go (let [response (<! (http/get (str (get-next-evaluation-path) experiment-id)
+                                   {:with-credentials? false}))]
+        (reset! current-evaluation (:body response)))))
+
+(defn prepare-next-evaluation! [experiment-id]
+  (update-current-evaluation! experiment-id))
+
+(defn submit-selected! [is-rewrite-preferred? experiment-id]
+  (go (<! (http/post (str (submit-evaluation-path) experiment-id)
+                                    {:with-credentials? false
+                                     :json-params {:is-rewrite-preferred? is-rewrite-preferred?}}))
+      (prepare-next-evaluation! experiment-id)))
+
+(defn submit-updated-selected! []
+  (submit-selected! true experiment))
+
+(defn submit-original-selected! []
+  (submit-selected! false experiment))
+
+
+(defn display-original-relations []
+  [:ul (map (fn [sentence-relations] 
+              [:li (display-sentence-relations sentence-relations)])
+            (:original-relations @current-evaluation))])
+
+(defn display-re-written-relations []
+  [:ul (map (fn [sentence-relations] 
+              [:li (display-sentence-relations sentence-relations)])
+            (:rewritten-relations @current-evaluation))])
+
+(defn display-sentence []
+  [:div (:original-sentence @current-evaluation)])
+
+(defn render-evaluation! []
+  (r/render [display-original-relations] (sel1 :#original-relations))
+  (r/render [display-re-written-relations] (sel1 :#re-written-relations))
+  (r/render [display-sentence] (sel1 :#original-sentence)))
+
 (dommy/listen! (sel1 :#submit-button) :click submit-re-written-sentence!)
+(dommy/listen! (sel1 :#select-updated-button) :click submit-updated-selected!)
+(dommy/listen! (sel1 :#select-original-button) :click submit-original-selected!)
 
 ;; -------------------------
 ;; Initialize app
@@ -94,7 +141,8 @@
 (defn mount-root []
   (render-extracted-relations!)
   (render-original-sentence!)
-  (prepare-next-sentence! experiment))
+  (prepare-next-sentence! experiment)
+  (prepare-next-evaluation! experiment))
 
 (defn init! [env-host]
   (reset! host env-host)
